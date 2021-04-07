60c4615af25cc4b70e83d63fef86bce2c3cf876c
Théo DELRIEU <theo.delrieu@tanker.io>
2021-04-07 14:12:11 +0200

    ci/ios: add build-and-test/prepare in run-ci + from-sources jobs

--- run-ci.py
+++ run-ci.py
@@ -6,17 +6,19 @@ from typing import Dict, List, Optional
 import argparse
 import os
 from pathlib import Path
+import shutil
 import sys
 
 import tankerci
 import tankerci.android
+import tankerci.conan
+from tankerci.conan import TankerSource
 import tankerci.git
 import tankerci.gitlab
 import cli_ui as ui
 
 
-def build_and_check() -> None:
-    tankerci.run("yarn", cwd="example")
+def build_and_test_android() -> None:
     tankerci.run(
         "yarn", "detox", "build", "--configuration", "android-ci", cwd="example"
     )
@@ -40,6 +42,46 @@ def build_and_check() -> None:
         )
 
 
+def prepare(sdk: str, tanker_source: TankerSource, tanker_ref: Optional[str]) -> None:
+    sdk_folder = f"sdk-{sdk}"
+    if "CI" in os.environ:
+        repos = [sdk_folder]
+        if tanker_source == TankerSource.SAME_AS_BRANCH:
+            repos.append("sdk-native")
+        workspace_path = tankerci.git.prepare_sources(repos=repos, clean=True)
+    else:
+        if tanker_source == TankerSource.SAME_AS_BRANCH:
+            ui.fatal("--use-tanker=same-as-branch can only be used by the CI")
+        workspace_path = Path.cwd().parent
+    sdk_path = workspace_path / sdk_folder
+
+    sdk_env = os.environ.copy()
+    sdk_env.pop("VIRTUAL_ENV", None)
+    tankerci.run(
+        "poetry", "install", cwd=sdk_path, env=sdk_env,
+    )
+    args = [
+        "poetry", "run", "python", "run-ci.py",
+        "build-and-test", f"--use-tanker={tanker_source.value}",
+    ]
+    if tanker_ref != None:
+        args.append(f"--tanker-ref={tanker_ref}")
+    tankerci.run(*args, cwd=sdk_path, env=sdk_env)
+    if sdk == "ios":
+        dest_path = Path.cwd() / "pod"
+        shutil.rmtree(dest_path, ignore_errors=True)
+        shutil.copytree(sdk_path / "pod", dest_path)
+
+
+def build_and_test(sdk: str) -> None:
+    tankerci.run("yarn")
+    if sdk == "android":
+        build_and_test_android()
+    else:
+        # TODO add real tests
+        tankerci.run("yarn", "--cwd", "example", "react-native", f"run-{sdk}", "--verbose")
+
+
 def main() -> None:
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(title="subcommands", dest="command")
@@ -53,6 +95,20 @@ def main() -> None:
     download_artifacts_parser.add_argument("--project-id", required=True)
     download_artifacts_parser.add_argument("--pipeline-id", required=True)
     download_artifacts_parser.add_argument("--job-name", required=True)
+
+    prepare_parser = subparsers.add_parser("prepare")
+    prepare_parser.add_argument("sdk", choices=["ios"])
+    prepare_parser.add_argument(
+        "--use-tanker",
+        type=tankerci.conan.TankerSource,
+        choices=[TankerSource.EDITABLE, TankerSource.DEPLOYED, TankerSource.SAME_AS_BRANCH],
+        default=TankerSource.EDITABLE,
+        dest="tanker_source",
+    )
+    prepare_parser.add_argument("--tanker-ref")
+
+    build_and_test_parser = subparsers.add_parser("build-and-test")
+    build_and_test_parser.add_argument("sdk", choices=["ios", "android"])
 
     args = parser.parse_args()
     command = args.command
@@ -69,8 +125,10 @@ def main() -> None:
             pipeline_id=args.pipeline_id,
             job_name=args.job_name,
         )
+    elif command == "prepare":
+        prepare(args.sdk, args.tanker_source, args.tanker_ref)
     elif command == "build-and-test":
-        build_and_check()
+        build_and_test(args.sdk)
     else:
         parser.print_help()
         sys.exit()
