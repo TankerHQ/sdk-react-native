c54a47c5d7b45296b3bff9208a831dcb378b6771
Timothée Isnard <timothee.isnard@tanker.io>
2021-04-16 15:02:44 +0200

    Remove some checks from extractEncryptionOptions

--- src/encryptionOptions.ts
+++ src/encryptionOptions.ts
@@ -26,18 +26,7 @@ export const extractEncryptionOptions = (
   if ('shareWithSelf' in options) {
     if (typeof options.shareWithSelf !== 'boolean') throw error;
     encryptionOptions.shareWithSelf = options.shareWithSelf;
-  } else {
-    encryptionOptions.shareWithSelf = true;
   }
-
-  if (
-    (!encryptionOptions.shareWithUsers ||
-      !encryptionOptions.shareWithUsers.length) &&
-    (!encryptionOptions.shareWithGroups ||
-      !encryptionOptions.shareWithGroups.length) &&
-    !encryptionOptions.shareWithSelf
-  )
-    throw new InvalidArgument('cannot encrypt and not share with anybody');
 
   return encryptionOptions;
 };
