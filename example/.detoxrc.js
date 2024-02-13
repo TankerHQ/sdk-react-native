2b473808d75343216318957e10955d9b5fba5f09
Timothée Isnard <timothee.isnard@tanker.io>
2024-02-13 15:24:22 +0100

    feat(ci): Run detox android on oldest and latest API level

--- example/.detoxrc.js
+++ example/.detoxrc.js
@@ -48,16 +48,28 @@ module.exports = {
         adbName: '.*'
       }
     },
-    emulator_x86_64: {
+    emulator_x86_64_oldest: {
       type: 'android.emulator',
       device: {
-        avdName: 'system-images-android-33-google_apis-x86_64'
+        avdName: 'system-images-android-21-google_apis-x86_64'
       }
     },
-    emulator_armv8: {
+    emulator_x86_64_latest: {
       type: 'android.emulator',
       device: {
-        avdName: 'system-images-android-33-google_apis-arm64-v8a'
+        avdName: 'system-images-android-34-google_apis-x86_64'
+      }
+    },
+    emulator_armv8_oldest: {
+      type: 'android.emulator',
+      device: {
+        avdName: 'system-images-android-21-google_apis-arm64-v8a'
+      }
+    },
+    emulator_armv8_latest: {
+      type: 'android.emulator',
+      device: {
+        avdName: 'system-images-android-34-google_apis-arm64-v8a'
       }
     }
   },
@@ -78,20 +90,36 @@ module.exports = {
       device: 'attached',
       app: 'android.release'
     },
-    'android.emu.x86_64.debug': {
-      device: 'emulator_x86_64',
+    'android.emu.x86_64.latest.debug': {
+      device: 'emulator_x86_64_latest',
       app: 'android.debug'
     },
-    'android.emu.x86_64.release': {
-      device: 'emulator_x86_64',
+    'android.emu.x86_64.oldest.debug': {
+      device: 'emulator_x86_64_oldest',
+      app: 'android.debug'
+    },
+    'android.emu.x86_64.latest.release': {
+      device: 'emulator_x86_64_latest',
       app: 'android.release'
     },
-    'android.emu.armv8.debug': {
-      device: 'emulator_armv8',
+    'android.emu.x86_64.oldest.release': {
+      device: 'emulator_x86_64_oldest',
+      app: 'android.release'
+    },
+    'android.emu.armv8.oldest.debug': {
+      device: 'emulator_armv8_oldest',
       app: 'android.debug'
     },
-    'android.emu.armv8.release': {
-      device: 'emulator_armv8',
+    'android.emu.armv8.latest.debug': {
+      device: 'emulator_armv8_latest',
+      app: 'android.debug'
+    },
+    'android.emu.armv8.oldest.release': {
+      device: 'emulator_armv8_oldest',
+      app: 'android.release'
+    },
+    'android.emu.armv8.latest.release': {
+      device: 'emulator_armv8_latest',
       app: 'android.release'
     }
   }
