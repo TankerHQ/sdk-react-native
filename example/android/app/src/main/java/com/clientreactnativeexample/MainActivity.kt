286fa5ae2f50c1e9082164bd4dc7712fab562fbc
Timothée Isnard <timothee.isnard@tanker.io>
2024-03-31 15:43:33 +0200

    fix: Workaround Detox bug on Android with New Architecture
    The Detox network synchronization class tries to find the
    React Native OkHttpClient by reflection, in a native java module of
    React Native.
    
    Unfortunately when the new architecture (specifically Fabric) is
    enabled, Detox looks for the HTTP client _before_ it is initialized.
    
    We disable the synchronization during the app load, then in the app
    initialization we force the React Native HTTP client to load,
    and finally we re-enable synchronization.
    At this point the HTTP client will have been created, and the
    Detox reflection nonsense will work.

--- example/android/app/src/main/java/com/clientreactnativeexample/MainActivity.kt
+++ example/android/app/src/main/java/com/clientreactnativeexample/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.clientreactnativeexample
 
+import android.os.Bundle
+import android.os.PersistableBundle
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
+import com.facebook.react.ReactInstanceManager
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.fabricEnabled
 import com.facebook.react.defaults.DefaultReactActivityDelegate
 
-class MainActivity : ReactActivity() {
+import com.facebook.react.modules.network.NetworkingModule
 
+
+class MainActivity : ReactActivity() {
   /**
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
@@ -19,4 +24,18 @@ class MainActivity : ReactActivity() {
    */
   override fun createReactActivityDelegate(): ReactActivityDelegate =
     DefaultReactActivityDelegate(this, mainComponentName, fabricEnabled)
+
+  override fun onResume() {
+    super.onResume()
+
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      // Detox can fail because the NetworkingModule is not initialized yet
+      // We force it to initialize here (as soon as the JS bundle is loaded,
+      // when the ReactContext becomes available)
+      reactInstanceManager.addReactInstanceEventListener {
+        val netModule = it.getNativeModule(NetworkingModule::class.java)
+        println("Networking module loaded: $netModule")
+      }
+    }
+  }
 }
