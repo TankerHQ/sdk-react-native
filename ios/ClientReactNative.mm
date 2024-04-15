e5fc493dc143dec98a36427d1e6284d1362c3c8a
Timothée Isnard <timothee.isnard@tanker.io>
2024-04-15 17:11:52 +0200

    feat(ios): Can start and check status

--- ios/ClientReactNative.mm
+++ ios/ClientReactNative.mm
@@ -65,6 +65,18 @@ RCT_REMAP_BLOCKING_SYNCHRONOUS_METHOD(create, id, createWithOptions:(nonnull NSD
     return @{@"ok": [self insertTankerInstanceInMap:tanker]};
 }
 
+RCT_REMAP_METHOD(start, startWithTankerHandle:(nonnull NSNumber*)handle identity:(nonnull NSString*)identity resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
+{
+    TKRTanker* tanker = [self.tankerInstanceMap objectForKey:handle];
+    if (!tanker)
+        return rejectInvalidHandle(reject, handle);
+    [tanker startWithIdentity:identity completionHandler:^(TKRStatus status, NSError * _Nullable err) {
+        if (err != nil)
+            return rejectWithError(reject, err);
+        resolve([NSNumber numberWithInt:(int)status]);
+    }];
+}
+
 RCT_REMAP_METHOD(stop, stopWithTankerHandle:(nonnull NSNumber*)handle resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject)
 {
     TKRTanker* tanker = [self.tankerInstanceMap objectForKey:handle];
@@ -76,6 +88,14 @@ RCT_REMAP_METHOD(stop, stopWithTankerHandle:(nonnull NSNumber*)handle resolver:(
         [self removeTankerInstanceInMap:handle];
         resolve(nil);
     }];
+}
+
+RCT_REMAP_BLOCKING_SYNCHRONOUS_METHOD(getStatus, id, getStatusWithTankerHandle:(nonnull NSNumber*)handle)
+{
+    TKRTanker* tanker = [self.tankerInstanceMap objectForKey:handle];
+    if (!tanker)
+        return invalidHandleError(handle);
+    return @{@"ok": [NSNumber numberWithInt:(int)tanker.status]};
 }
 
 @end
