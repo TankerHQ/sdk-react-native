a2904118394b3d072c38deb78012f55e1150b798
Quentin Vernot <quentin.vernot@doctolib.com>
2024-12-11 17:54:32 +0100

    chore(ios): add clang-format config and run it
    This was done using clang-format `14.0.0-1ubuntu1.1`, and the .clang-format config was copied straight from sdk-ios.

--- ios/Utils+Private.h
+++ ios/Utils+Private.h
@@ -1,20 +1,21 @@
+#import "Tanker/TKRError.h"
 #import "Tanker/TKRTanker.h"
 #import "Tanker/TKRTankerOptions.h"
-#import "Tanker/TKRError.h"
 
 #import <React/RCTBridgeModule.h>
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
-TKRTankerOptions* _Nonnull dictToTankerOptions(NSDictionary<NSString*, id>* _Nonnull optionsDict);
-NSDictionary* _Nonnull invalidHandleError(unsigned handle);
-NSString* _Nonnull errorCodeToString(TKRError err);
-void rejectInvalidHandle(RCTPromiseRejectBlock _Nonnull reject, unsigned handle);
-void rejectInvalidVerificationDict(RCTPromiseRejectBlock _Nonnull reject);
-void rejectWithInternalError(RCTPromiseRejectBlock _Nonnull reject, NSString *_Nonnull msg);
-void rejectWithError(RCTPromiseRejectBlock _Nonnull reject, NSError* _Nonnull err);
+  TKRTankerOptions* _Nonnull dictToTankerOptions(NSDictionary<NSString*, id>* _Nonnull optionsDict);
+  NSDictionary* _Nonnull invalidHandleError(unsigned handle);
+  NSString* _Nonnull errorCodeToString(TKRError err);
+  void rejectInvalidHandle(RCTPromiseRejectBlock _Nonnull reject, unsigned handle);
+  void rejectInvalidVerificationDict(RCTPromiseRejectBlock _Nonnull reject);
+  void rejectWithInternalError(RCTPromiseRejectBlock _Nonnull reject, NSString* _Nonnull msg);
+  void rejectWithError(RCTPromiseRejectBlock _Nonnull reject, NSError* _Nonnull err);
 
 #ifdef __cplusplus
 };
